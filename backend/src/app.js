f09440aad54f669d580f759c2d92a7ddf68b6944
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2026-01-20 20:46:19 +0530

    configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 //Middlewares
 app.use(express.json({ limit: "16kb" }));
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true
+}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
