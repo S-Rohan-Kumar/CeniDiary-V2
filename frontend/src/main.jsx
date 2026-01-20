03a42308d8fb242cadff4db124686c75a22ec2b1
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2026-01-20 21:07:34 +0530

    frontend configs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,12 @@ import App from "./App.jsx";
 import { MovieProvider } from "./contexts/MovieContext.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import axios from "axios";
+axios.defaults.baseURL = import.meta.env.PROD 
+  ? "https://cenidiary-v2.onrender.com" 
+  : "";
+
+axios.defaults.withCredentials = true;  
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
