45f4166c1dadbb0c16fa50073305bc83b96628d3
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2026-01-21 07:55:42 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,7 @@ import { MovieProvider } from "./contexts/MovieContext.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import axios from "axios";
-axios.defaults.baseURL = import.meta.env.PROD 
-  ? "https://cenidiary-v2.onrender.com" 
-  : "";
+axios.defaults.baseURL = "https://cenidiary-v2.onrender.com"
 
 axios.defaults.withCredentials = true;  
 
