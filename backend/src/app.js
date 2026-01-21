f183407fda622a8bf626bed9ed2355a61a808737
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2026-01-21 07:57:48 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(cors({
     origin: process.env.CORS_ORIGIN, 
-    credentials: true
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
