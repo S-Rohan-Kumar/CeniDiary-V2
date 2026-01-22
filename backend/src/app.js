27e82011f4b8d806bbd1ded9280bd1d046c50c47
S-Rohan-Kumar <rohankumar04102005@gmail.com>
2026-01-22 09:42:56 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 //Middlewares
 app.use(express.json({ limit: "16kb" }));
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, 
+    origin: {
+        "https://ceni-diary-v2.vercel.app",
+        "http://localhost:5173"
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
